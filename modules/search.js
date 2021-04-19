143549183df29c912767c62d5008bfcea44656e0
MacixxPL <63600410+MacixxPL@users.noreply.github.com>
2021-04-19 22:20:16 +0200

    fixes to ui

--- modules/search.js
+++ modules/search.js
@@ -1,11 +1,13 @@
-document.querySelector('.question_essence').childNodes.forEach(e => {
-    
-    if(e.tagName === "P") {
+document.querySelector(".question_essence").childNodes.forEach((e) => {
+    if (e.tagName === "P") {
         const searchParams = new URLSearchParams();
         searchParams.append("q", e.textContent);
-        e.setAttribute('onclick', `window.open('https://www.google.com/search?${searchParams.toString()}')`);
-        e.style['text-decoration-line'] = "underline";
-        e.style['font-size'] = "1.3rem";
-    };
-
+        e.setAttribute(
+            "onclick",
+            `window.open('https://www.google.com/search?${searchParams.toString()}')`
+        );
+        e.style["text-decoration-line"] = "underline";
+        e.style["cursor"] = "pointer";
+        e.style["font-size"] = "1.3rem";
+    }
 });
