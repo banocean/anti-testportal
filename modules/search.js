042329509591a5ab6095ce5a48a8fe011d8ac0d4
oceaann <kontakt.ocaen@gmail.com>
2021-04-22 17:29:08 +0200

    Google image search + cursor pointer

--- modules/search.js
+++ modules/search.js
@@ -1,11 +1,32 @@
-document.querySelector('.question_essence').childNodes.forEach(e => {
+const questions = document.querySelector('.question_essence')
+
+questions.childNodes.forEach(e => {
+
+    if(e.childNodes && e.childNodes[0] && e.tagName) {
+
+        e.childNodes.forEach(e => {
+
+            if(e.tagName === "IMG") {
+        
+                const searchParams = new URLSearchParams();
+                searchParams.append("igu", "1");
+                searchParams.append("image_url", e.attributes['data-src'].value || e.src);
+                e.setAttribute('onclick', `window.open("https://www.google.com/searchbyimage?${searchParams.toString()}")`);
+                e.style["cursor"] = "pointer";
+
+            };
     
-    if(e.tagName === "P") {
+        });
+    
+    } else if(e.tagName === "P") {
+
         const searchParams = new URLSearchParams();
         searchParams.append("q", e.textContent);
         e.setAttribute('onclick', `window.open('https://www.google.com/search?${searchParams.toString()}')`);
         e.style['text-decoration-line'] = "underline";
         e.style['font-size'] = "1.3rem";
-    };
-
+        e.style["cursor"] = "pointer";
+    
+    };    
+    
 });
